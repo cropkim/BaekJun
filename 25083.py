4f23852da4f56332c76c3d7f3a9d15d2ffeaea22
JunTaeKims <kjt0518@gmail.com>
2022-12-28 17:09:17 +0900

    25083.py

--- 25083.py
+++ 25083.py
@@ -1,5 +1,7 @@
-f = open("25083.txt","r")
-lines = f.readlines()
-for line in lines:
-    print(line)
-f.close()
+import pandas as pd
+URL = "https://raw.githubusercontent.com/cropkim/BaekJun/master/25083.txt"
+data = pd.read_csv(URL)
+# f = open("25083.txt","r")
+# lines = f.readlines()
+print(data)
+
