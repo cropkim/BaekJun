7b57da65b9992ffc4cbb86da2f7ef7c65a622f84
JunTaeKims <kjt0518@gmail.com>
2022-12-31 21:40:29 +0900

    25304백준 update

--- 25304.py
+++ 25304.py
@@ -1,9 +1,9 @@
 li = [int(input()) for _ in range(2)]
-S=0
+
 for i in range (li[1]):
     a, b = map(int,input().split())
-    S = S+ a*b
+    li[0] = li[0]-(a*b)
     
-if li[0] == S:
+if li[0] == 0:
     print("Yes")
 else:print("No")    
